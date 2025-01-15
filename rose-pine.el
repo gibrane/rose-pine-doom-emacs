83aeff219c629b67344df28747437ea49d45aa8a
Gibran Essa <gessa@gibran-dev.nvidia.com>
2025-01-15 10:49:38 -0800

    treat this like a package

---
+++ rose-pine.el
@@ -0,0 +1,28 @@
+;;; rose-pine.el -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Gibran Essa
+;;
+;; Author: Gibran Essa <gessa@gibran-dev.nvidia.com>
+;; Maintainer: Gibran Essa <gessa@gibran-dev.nvidia.com>
+;; Created: January 15, 2025
+;; Modified: January 15, 2025
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/gessa/rose-pine
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+(load! "doom-rose-pine-dawn-theme.el")
+(load! "doom-rose-pine-moon-theme.el")
+(load! "doom-rose-pine-theme.el")
+
+
+(provide 'rose-pine)
+;;; rose-pine.el ends here
